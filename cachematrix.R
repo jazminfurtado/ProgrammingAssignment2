6d6bd4151b617b833be2adbc4226a54693b00239
Jazmin Furtado <c16jazmin.furtado@usafa.edu>
2015-01-23 01:26:49 -0700

    this is the new R file for cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This code outputs the inverse of a matrix
 
-## Write a short comment describing this function
+#makeMatrix creates a list that contains the values of setting and getting the 
+##matrix and inverse
+makeCacheMatrix<-function(x=matrix()){
+  m <- NULL
+  #set, get, setinverse, getinverse are functions for the inputted matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x    
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#cacheSolve uses the list and inverts the matrix,'x' 
+##if it has not already been inverted
+cacheSolve<-function(x, ...){
+  m <- x$getinverse()
+  #if there is a value in m, return it; if not, find inverse
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+
+make.power<- function(n){
+  pow<-function(x){
+    x^n
+  }
+  pow
 }
+
+cube<-make.power(3)
+square<-make.power(2)
+cube(3)
+square(3)
